32aef0331659cce4fc028c26c6ea73370d8edd28
mcisiel <mart.ski.59@gmail.com>
2018-09-25 15:18:05 +0200

    ADD : Affichage de POIs sur la carte

--- routes/map.js
+++ routes/map.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+var locations = [];
+locations.push({'latitude': '51.509', 'longitude': '-0.08'});
+locations.push({'latitude': '50.509', 'longitude': '10.08'});
+
 /* GET map page. */
 router.get('/', function(req, res, next) {
-  res.render('map', { title: 'Exemple map' });
+  res.render('map', { title: 'Exemple map', locations: locations });
 });
 
 module.exports = router;
