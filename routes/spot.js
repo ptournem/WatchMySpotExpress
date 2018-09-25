717ae68284352b5e2ceaa8d87b48d713c8b7b098
wendy <wendynorbert.toradiha0gmail.com>
2018-09-25 17:29:33 +0200

    ADD : Ajout de la route d'un spot avec un param

--- routes/spot.js
+++ routes/spot.js
@@ -4,7 +4,7 @@ var router = express.Router();
 var viewTitle;
 
 function returnRender(){
-  return {
+  var obj =  {
       title: viewTitle,
       home: 'Accueil',
       search: 'Recherche',
@@ -13,14 +13,22 @@ function returnRender(){
       application: 'Une application pour les surfeurs à la recherche de nouveaux spot de surf!',
       tchat: 'Chat en ligne'
     };
+
+
+    return obj;
 }
 
 
-// Route pour la vue spot
+// Route pour la vue spot (sans param => Mes spots favoris)
 router.get('/', function(req, res, next) {
   viewTitle = "Mes Spots";
   res.render('spot', returnRender());
 });
 
+// Route pour la vue d'un spot en particulier (avec un identifiant en param)
+router.get('/:id', function(req, res, next) {
+  viewTitle = "Spot : ";
+  res.render('spot', returnRender());
+});
 
 module.exports = router;
