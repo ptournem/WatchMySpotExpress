23f98f6f7fad2d80e18c88ce4575aa94b41d7551
Paul Tournemaine <paultournemaine@gmail.com>
2018-09-26 10:34:19 +0200

    ADD : middleware for express-session

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,7 +22,18 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  key : 'session_id',
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req,res,next){
+  console.log(req.session);
+  next();
+})
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
